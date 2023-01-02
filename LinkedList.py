1b858802d03f4b23c1d8bdb8c1ae7ee4f34a5c7a
Chris Sefcik <cdsefcik@gmail.com>
2023-01-02 13:27:10 -0500

    Add files via upload

---
+++ LinkedList.py
@@ -0,0 +1,12 @@
+class LinkedList:
+    
+    def __init__(self):
+        self.head = None
+        
+        
+class Node:
+    
+    def __init(self, data):
+        self.next = None
+        self.data = data
+        
